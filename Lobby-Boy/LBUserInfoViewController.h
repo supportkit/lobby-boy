2dc35abf12ffe35a74446a0e1177a4797da5c0ca
mike gozzo <mgozzo@gmail.com>
2015-04-23 13:10:59 -0400

    Initial Commit

--- Lobby-Boy/LBUserInfoViewController.h
+++ Lobby-Boy/LBUserInfoViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LBUserInfoViewController.h
 //  Lobby-Boy
 //
 //  Created by Mike on 2015-04-23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LBUserInfoViewController : UIViewController
 
 @end
-
---
+++ Lobby-Boy/LBUserInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  LBUserInfoViewController.m
+//  Lobby-Boy
+//
+//  Created by Mike on 2015-04-23.
+//  Copyright (c) 2015 SupportKit.io. All rights reserved.
+//
+
+#import "LBUserInfoViewController.h"
+
+@interface LBUserInfoViewController ()
+
+@end
+
+@implementation LBUserInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
