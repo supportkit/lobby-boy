ddeff4c2336a3bfc3a9ac8171811faab39cf5a43
Jean-Philippe Joyal <jpjoyal@gmail.com>
2015-04-23 14:20:43 -0400

    update cocoa pod to 0.36.4

--- Pods/Target Support Files/Pods/Pods-resources.sh
+++ Pods/Target Support Files/Pods/Pods-resources.sh
@@ -6,6 +6,14 @@ mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
 > "$RESOURCES_TO_COPY"
 
+XCASSET_FILES=()
+
+realpath() {
+  DIRECTORY=$(cd "${1%/*}" && pwd)
+  FILENAME="${1##*/}"
+  echo "$DIRECTORY/$FILENAME"
+}
+
 install_resource()
 {
   case $1 in
@@ -36,6 +44,8 @@ install_resource()
       xcrun mapc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcmappingmodel`.cdm"
       ;;
     *.xcassets)
+      ABSOLUTE_XCASSET_FILE=$(realpath "${PODS_ROOT}/$1")
+      XCASSET_FILES+=("$ABSOLUTE_XCASSET_FILE")
       ;;
     /*)
       echo "$1"
@@ -47,36 +57,62 @@ install_resource()
       ;;
   esac
 }
-          install_resource "PaymentKit/PaymentKit/Resources/Cards/amex.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/amex@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/diners.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/diners@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/discover.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/discover@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/visa.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/Cards/visa@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/gradient@2x.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/textfield.png"
-                    install_resource "PaymentKit/PaymentKit/Resources/textfield@2x.png"
-                    install_resource "SupportKit/SupportKit.bundle"
-          
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/amex.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/amex@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/diners.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/diners@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/discover.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/discover@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/visa.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/visa@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/gradient@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/textfield.png"
+  install_resource "PaymentKit/PaymentKit/Resources/textfield@2x.png"
+  install_resource "SupportKit/SupportKit.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/amex.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/amex@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc-amex@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/cvc@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/diners.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/diners@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/discover.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/discover@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/jcb@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/mastercard@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/placeholder@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/visa.png"
+  install_resource "PaymentKit/PaymentKit/Resources/Cards/visa@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/gradient@2x.png"
+  install_resource "PaymentKit/PaymentKit/Resources/textfield.png"
+  install_resource "PaymentKit/PaymentKit/Resources/textfield@2x.png"
+  install_resource "SupportKit/SupportKit.bundle"
+fi
+
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
   rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${INSTALL_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
 rm -f "$RESOURCES_TO_COPY"
 
-if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ `find . -name '*.xcassets' | wc -l` -ne 0 ]
+if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ -n "$XCASSET_FILES" ]
 then
   case "${TARGETED_DEVICE_FAMILY}" in
     1,2)
@@ -92,5 +128,14 @@ then
       TARGET_DEVICE_ARGS="--target-device mac"
       ;;
   esac
-  find "${PWD}" -name "*.xcassets" -print0 | xargs -0 actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+
+  # Find all other xcassets (this unfortunately includes those of path pods and other targets).
+  OTHER_XCASSETS=$(find "$PWD" -iname "*.xcassets" -type d)
+  while read line; do
+    if [[ $line != "`realpath $PODS_ROOT`*" ]]; then
+      XCASSET_FILES+=("$line")
+    fi
+  done <<<"$OTHER_XCASSETS"
+
+  printf "%s\0" "${XCASSET_FILES[@]}" | xargs -0 xcrun actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
