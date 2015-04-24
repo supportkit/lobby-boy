26d4bda9986b294979524a0ec4b9de087d3a5de0
Jean-Philippe Joyal <jpjoyal@gmail.com>
2015-04-24 09:42:39 -0400

    watkit extension

---
+++ Lobby-Boy WatchKit Extension/LBProductImageTableRowController.m
@@ -0,0 +1,14 @@
+//
+//  LBProductImageTableRowController.m
+//  Lobby-Boy
+//
+//  Created by Jean-Philippe Joyal on 2015-04-23.
+//  Copyright (c) 2015 SupportKit.io. All rights reserved.
+//
+
+#import "LBProductImageTableRowController.h"
+
+@implementation LBProductImageTableRowController
+
+
+@end
